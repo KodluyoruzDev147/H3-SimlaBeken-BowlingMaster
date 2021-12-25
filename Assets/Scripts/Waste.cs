72087faa5d821a2e1ee94e755d0b284b77bee249
Simla Beken <38881186+Simla4@users.noreply.github.com>
2021-12-25 03:10:53 +0300

    losing condition

--- Assets/Scripts/Waste.cs
+++ Assets/Scripts/Waste.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Waste : MonoBehaviour
 {
-    [SerializeField] private GameObject game;
-
-
+    [SerializeField] private GameObject UI;
 
     //To finish the game when it touches the grooves on the right and left of the bowling alley.
     private void OnCollisionEnter(Collision collision)
     {
 
+
         if (collision.gameObject.tag == "Gutter")
         {
-
-            game.SetActive(false);
+            SceneManager.LoadScene("SampleScene");
+            UI.SetActive(true);
 
         }
     }

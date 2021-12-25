86976545f6f324c479a38f2697a8859c9ce97f4e
Simla Beken <38881186+Simla4@users.noreply.github.com>
2021-12-25 03:09:42 +0300

    score counter

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
 
     private int score = 0;
+    [SerializeField] private Text txt;
 
     private void OnCollisionEnter(Collision collision)
     {
-       
-            if (collision.gameObject.tag == "Pin")
-            {
 
-                score += 1;
-                Debug.Log(score);
-                Handheld.Vibrate();
+        if (collision.gameObject.tag == "Pin")
+        {
 
-            }        
+            score += 1;
+            txt.text = score.ToString();
+            //Debug.Log(score);
+            Handheld.Vibrate();
+
+        }
     }
 }
